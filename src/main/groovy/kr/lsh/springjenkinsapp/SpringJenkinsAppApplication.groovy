48d6bccf1e96c96d6b9840ac23c5e2702b5df23c
lsh9084 <sanghun1101088@gmail.com>
2024-10-11 11:22:55 +0900

    .

---
+++ src/main/groovy/kr/lsh/springjenkinsapp/SpringJenkinsAppApplication.groovy
@@ -0,0 +1,13 @@
+package kr.lsh.springjenkinsapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringJenkinsAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication, args)
+    }
+
+}
